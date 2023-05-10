1e69e50e75ea9ecf79a00ac9b892345d725e210b
Justin Lopato <justin@lopato.org>
2023-05-10 10:25:12 -0500

    wlknfsl

--- src/app.py
+++ src/app.py
@@ -1,6 +1,20 @@
-import einops
+import os
+import time
 import torch
-import cv2
+import einops
+import random
+import numpy as np
+import torch.optim as optim
+import torch.nn as nn
+import torch.onnx as onnx
+import torchvision.models as models
+from natsort import natsorted
+from tensorboardX import SummaryWriter
+from datetime import datetime
+from argparse import ArgumentParser
+from torch.utils.data import DataLoader
+from data import *
+from utils import warp, update_train_log, write_train_log, update_eval_log, write_eval_log, print_eval_log
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -8,47 +22,42 @@ app = Flask(__name__)
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    verbose = args.verbose
-  device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-  # DATASET
-  images = [x for x in natsorted(os.listdir(args.dir)) if ('.jpg' in x) or ('.png' in x)]
+  image = "1.50.png"
 
   # MODEL
   model_stn = models.resnet50()
   model_stn.fc = nn.Linear(2048, 8)
   model = models.resnet50()
   model.fc = nn.Linear(2048, 720)
-  resume_path = './models/{}.pth'.format(verbose)
-  stn_resume_path = './models/{}_st.pth'.format(verbose)
+  resume_path = './models/full+++.pth'
+  stn_resume_path = './models/full+++_st.pth'
   model.load_state_dict(torch.load(resume_path, map_location=torch.device('cpu')))
   model_stn.load_state_dict(torch.load(stn_resume_path, map_location=torch.device('cpu')))
-  model_stn.to(device)
-  model.to(device)
+  model_stn.to('cpu')
+  model.to('cpu')
 
-  for count in range(len(images)):
-    with torch.no_grad():
-      model.eval()
-      model_stn.eval()
+  with torch.no_grad():
+    model.eval()
+    model_stn.eval()
 
-      #MODEL
-      img = cv2.imread(os.path.join(args.dir, images[count]))
-      img = cv2.resize(img, (224, 224))/255.
-      img = einops.rearrange(img, 'h w c -> c h w')
-      img = torch.Tensor(img)
-      img = img.float().to(device)
-      img = torch.unsqueeze(img, 0)
+    #MODEL
+    img = cv2.imread(os.path.join("./data/demo", image))
+    img = cv2.resize(img, (224, 224))/255
+    img = einops.rearrange(img, 'h w c -> c h w')
+    img = torch.Tensor(img)
+    img = img.float().to('cpu')
+    img = torch.unsqueeze(img, 0)
 
-      pred_st = model_stn(img)
-      pred_st = torch.cat([pred_st,torch.ones(1,1).to(device)], 1)
-      Minv_pred = torch.reshape(pred_st, (-1, 3, 3))
-      img_ = warp(img, Minv_pred)
-      pred = model(img_)
+    #STN
+    pred_st = model_stn(img)
+    pred_st = torch.cat([pred_st,torch.ones(1,1).to('cpu')], 1)
+    Minv_pred = torch.reshape(pred_st, (-1, 3, 3))
+    img_ = warp(img, Minv_pred)
+    pred = model(img_)
 
-      #top 3 predictions
-      max_pred = torch.argsort(pred, dim=1, descending=True)
-      max_pred = max_pred[0,:3]
-      max_h = max_pred[0] // 60
-      max_m = max_pred[0] % 60
+    #top 3 predictions
+    max_pred = torch.argsort(pred, dim=1, descending=True)
+    max_h = max_pred[0] // 60
+    max_m = max_pred[0] % 60
 
-      print(f"[{images[count]}] is roughly {max_h.cpu().numpy()}:{max_m.cpu().numpy():0>2}.\n")
+    print(f"[{image}] is roughly {max_h[0].cpu().numpy()}:{max_m[0].cpu().numpy():0>2} ({max_pred[0][0].cpu().numpy()}).\n")
--- src/predict.py
+++ src/predict.py
@@ -16,9 +16,8 @@ from torch.utils.data import DataLoader
 from data import *
 from utils import warp, update_train_log, write_train_log, update_eval_log, write_eval_log, print_eval_log
 
-def main(args):
-  verbose = args.verbose
-  device = 'cuda' if torch.cuda.is_available() else 'cpu'
+def main():
+  device = 'cpu'
 
   # DATASET
   images = ["1.50.png"]
@@ -28,51 +27,33 @@ def main(args):
   model_stn.fc = nn.Linear(2048, 8)
   model = models.resnet50()
   model.fc = nn.Linear(2048, 720)
-  resume_path = './models/{}.pth'.format(verbose)
-  stn_resume_path = './models/{}_st.pth'.format(verbose)
+  resume_path = './models/full+++.pth'
+  stn_resume_path = './models/full+++_st.pth'
   model.load_state_dict(torch.load(resume_path, map_location=torch.device('cpu')))
   model_stn.load_state_dict(torch.load(stn_resume_path, map_location=torch.device('cpu')))
   model_stn.to(device)
   model.to(device)
 
-  for count in range(len(images)):
-    with torch.no_grad():
-      model.eval()
-      model_stn.eval()
+  with torch.no_grad():
+    model.eval()
+    model_stn.eval()
 
-      #MODEL
-      img = cv2.imread(os.path.join(args.dir, images[count]))
-      print(f"Predicting {images[count]}...")
-      img = cv2.resize(img, (224, 224))/255
-      print(img.shape, "\n")
-      print(img, "\n")
-      img = einops.rearrange(img, 'h w c -> c h w')
-      print(img.shape, "\n")
-      print(img, "\n")
-      img = torch.Tensor(img)
-      print(img.shape, "\n")
-      print(img, "\n")
-      img = img.float().to(device)
-      img = torch.unsqueeze(img, 0)
-      print(img.shape, "\n")
-      print(img, "\n")
-      pred = model(img)
+    #MODEL
+    img = cv2.imread(os.path.join("./data/demo", images[0]))
+    img = cv2.resize(img, (224, 224))/255
+    img = einops.rearrange(img, 'h w c -> c h w')
+    img = torch.Tensor(img)
+    img = img.float().to(device)
+    img = torch.unsqueeze(img, 0)
+    pred = model(img)
 
-      #top 3 predictions
-      max_pred = torch.argsort(pred, dim=1, descending=True)
-      max_h = max_pred[0] // 60
-      max_m = max_pred[0] % 60
+    #top 3 predictions
+    max_pred = torch.argsort(pred, dim=1, descending=True)
+    max_h = max_pred[0] // 60
+    max_m = max_pred[0] % 60
 
-      print(f"[{images[count]}] is roughly {max_h[0].cpu().numpy()}:{max_m[0].cpu().numpy():0>2} ({max_pred[0][0].cpu().numpy()}).\n")
-      
-      img = einops.rearrange(img[0], 'c h w -> h w c').cpu().numpy()[:,:,::-1] * 255 
-      cv2.imwrite('./data/img{}.png'.format(count), img)
+    print(f"[{images[0]}] is roughly {max_h[0].cpu().numpy()}:{max_m[0].cpu().numpy():0>2} ({max_pred[0][0].cpu().numpy()}).\n")
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument('--verbose', type=str, default='full+++')
-    parser.add_argument('--dir', type=str, default='./data/demo')
-
-    args = parser.parse_args()
-    main(args)
+  main()
